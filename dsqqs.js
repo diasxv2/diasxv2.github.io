60a35a713d7a99409e67251767051e2bf76390ac
diasxv2 <98944239+diasxv2@users.noreply.github.com>
2025-11-28 20:07:44 +0100

    updatedata

---
+++ dsqqs.js
@@ -0,0 +1,34 @@
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    const rotateX = (-y / 20).toFixed(2);
+    const rotateY = (x / 20).toFixed(2);
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0)";
+  });
+});
+
+document.querySelector(".contact-box").addEventListener("submit", e => {
+    e.preventDefault();
+    alert("Message envoyé !");
+});
+
+const music = document.getElementById("bgMusic");
+const btn = document.getElementById("musicBtn");
+
+btn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    btn.textContent = "🔊";
+  } else {
+    music.pause();
+    btn.textContent = "🔇";
+  }
+});
